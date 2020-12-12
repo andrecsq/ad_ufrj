7b42378388d298686b1a21303fa00725c390b0ee
Andre Queiroz <andrecsq1@gmail.com>
2020-12-12 19:13:02 -0300

    Added exact calculations

--- chapter1/32.py
+++ chapter1/32.py
@@ -1,6 +1,5 @@
 import random
 import numpy as np
-from numpy.lib.twodim_base import tri
 
 def run_simulation(n, number_of_coins, p):
     print(f"Flipping {number_of_coins} coins with probability {p} and {n} samples")
@@ -12,33 +11,33 @@ def run_simulation(n, number_of_coins, p):
         trials[i] = float(r.sum())
     return trials
 
-def print_report_prob(trials):
+def print_report_prob(trials, number_of_coins, p):
     prob_simulation = (trials == 1).sum()/len(trials)
     print(f"P(X=1) by simulation: {prob_simulation}")
-    # prob_exact = 3/8
-    # print(f"P(X=1) exact: {prob_exact}")
-    # print(f"Difference: {round(abs(prob_exact-prob_simulation), 5)}")
+    prob_exact = number_of_coins*p*(1-p)**(number_of_coins-1)
+    print(f"P(X=1) exact: {prob_exact}")
+    print(f"Error: {round(abs(prob_exact-prob_simulation), 5)}")
     
-def print_report_ev(trials):
-    ev_simulation = trials.sum()/(len(trials))
+def print_report_ev(trials, number_of_coins, p):
+    ev_simulation = trials.mean()
     print(f"E(X) by simulation: {ev_simulation}")
-    # ev_exact = 1.5
-    # print(f"E(X) exact: {ev_exact}")
-    # print(f"Error: {round(abs(ev_exact-ev_simulation), 5)}")
+    ev_exact = number_of_coins*p
+    print(f"E(X) exact: {ev_exact}")
+    print(f"Error: {round(abs(ev_exact-ev_simulation), 5)}")
     
-def print_report(trials):    
-    print_report_prob(trials)
-    print_report_ev(trials)
+def print_report(trials, number_of_coins, p):    
+    print_report_prob(trials, number_of_coins, p)
+    print_report_ev(trials, number_of_coins, p)
 
 n = 1000000
 number_of_coins = 3
 p_fair = 1/2
 trials = run_simulation(n, number_of_coins, p_fair)
-print_report(trials)
+print_report(trials, number_of_coins, p_fair)
 
 print()
 
 p_unfair = 3/4
 trials = run_simulation(n, number_of_coins, p_unfair)
-print_report(trials)
+print_report(trials, number_of_coins, p_unfair)
 
